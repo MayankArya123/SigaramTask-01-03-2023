823c4d5ed89105a8d059cdcf83a97e1197e1e21c
MayankArya123 <mayanksunny786@gmail.com>
2023-03-01 22:33:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  var count = 0;
+
+  const setColor = (e) => {
+    const prev = document.querySelectorAll(".div.red.prev");
+
+    if (prev.length > 0) {
+      prev[0].classList.remove("red");
+      prev[0].classList.remove("prev");
+    }
+    const reds = document.querySelectorAll(".div.red");
+    if (reds.length > 0) {
+      reds[0].classList.add("prev");
+    }
+
+    e.target.classList.add("red");
+
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div name="1" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="2" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="3" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="4" className="div " onClick={(e) => setColor(e)}></div>
+
+      <div name="5" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="6" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="7" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="8" className="div " onClick={(e) => setColor(e)}></div>
+
+      <div name="9" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="10" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="11" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="12" className="div " onClick={(e) => setColor(e)}></div>
+
+      <div name="13" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="14" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="15" className="div " onClick={(e) => setColor(e)}></div>
+      <div name="16" className="div " onClick={(e) => setColor(e)}></div>
     </div>
   );
 }
